6f72c9c93f2bae680333c8fa74cb0c3b7c24ebce
EAScript <76826198+EAScript@users.noreply.github.com>
2021-03-29 00:01:50 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup,find_packages
 setup(
   name = 'EAMetatrader',         # How you named your package folder (MyLib)
-  packages = ['EAMetatrader'],   # Chose the same as "name"
+  packages = find_packages(),   # Chose the same as "name"
   version = '1.0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Metatrader library for python',   # Give a short description about your library
   author = 'EAScript',                   # Type in your name
   author_email = 'ehsanakbariea8@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/EAScript/EAMT',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/EAScript/EAMT/archive/refs/tags/1.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/EAScript/EAMT/archive/refs/tags/1.0.2.tar.gz',    # I explain this later on
   keywords = ['EAMetatrader', 'Metatrader', 'Metatrader4', 'Metatrader5', 'python+metatrader', 'MtApi', 'mql4', 'mql5'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pythonnet',
       ],
   include_package_data=True,
-  package_data={"EAMetatrader.Client": ['MtApi.dll','Newtonsoft.Json.dll'],
-                "EAMetatrader.Using": ['System.Drawing.dll','WindowsBase.dll']},
+  package_data={'EAMetatrader': ['Client/MtApi.dll', 'Client/Newtonsoft.Json.dll', 'Using/System.Drawing.dll', 'Using/WindowsBase.dll']},
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
